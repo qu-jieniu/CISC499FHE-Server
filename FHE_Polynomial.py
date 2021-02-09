cc454e19d4ddfbee26fefb9cd92a6099466cbaab
Aidan Moffat <16ajbm@queensu.ca>
2021-02-09 17:15:23 -0500

    basic Polynomial class

---
+++ FHE_Polynomial.py
@@ -0,0 +1,62 @@
+class FHE_Polynomial:
+
+    def __init__(self,*coeffs):
+        self.coefficients = list(coeffs)
+    
+    def __repr__(self):
+        return "Polynomial" + str(tuple(self.coefficients)) 
+
+    def getCoefficients(self):
+        return self.coefficients # return as list
+    
+    def __str__(self):
+        def x_expr(deg):
+            if deg == 0:
+                res = ""
+            if deg ==1:
+                res = "x"
+            else:
+                res = "x^"+str(deg)
+            return res
+    
+        deg = len(self.coefficients) - 1
+        print(deg)
+        res = ""
+
+        for i in range(0,deg+1):
+            coeff = self.coefficients[i]
+        
+            if deg-i != 0:
+                if abs(coeff) == 1:
+                    if coeff < 0:
+                        res += "-x^"+str(deg-i)
+                    else:
+                        res += "+x^"+str(deg-i)
+                elif coeff != 0:
+                    if coeff < 0:
+                        res += "-" + str(abs(coeff)) + "x^" + str(deg-i)
+                    else:
+                        res += "+" + str(abs(coeff)) + "x^" + str(deg-i)
+            else:
+                if coeff != 0:
+                    if coeff > 0:
+                        res += "+" + str(coeff)
+                    else:
+                        res += "-" + str(abs(coeff))
+        return res.lstrip('+')
+
+    def __call__(self,x):
+        res = 0
+        for index, coeff in enumerate(self.coefficients[::-1]):
+            res += coeff * x**index
+        return res
+
+
+
+
+poly1 = FHE_Polynomial(1,2,3,4)
+poly2 = FHE_Polynomial(-2,-4,3,-1)
+
+print(str(poly1))
+print(str(poly2))
+print(poly2(6))
--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 from FHE_Integer import *
 from FHE_Client import *
 from FHE_Server import *
+from FHE_Polynomial import *
 
 
 client = FHE_Client()
 m = client.m #modulo
 p = client.p #padding
-
+print("m: "+ m)
+print("p: "+ p)
 
 operation = '*'
 x1 = FHE_Integer(1330, m, p, operation)
 x2 = FHE_Integer(394, m, p, operation)
 x1_prime, q1 = x1.getContext()
+print("x1_prime: "+ x1_prime)
+print("q1: "+q1)
 x2_prime, q2 = x2.getContext()
-
+print("q2: "+ q2)
 server = FHE_Server()
 x12_p_plus = server.calc(x1_prime, x2_prime, operation)
 
